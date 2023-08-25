c784c56a3134b2c262688b98eaff563ff82ddbf3
yb-niu <niuyanbiao@126.com>
2023-08-25 11:41:20 +0800

    UpdateMap.m

--- UpdateMap.m
+++ UpdateMap.m
@@ -9,17 +9,5 @@ function [scaleFactor, newMap] = UpdateMap(currentStep,pathLength,totalMoves,dir
     % Target move direction (totalMoves in the total path)
     move = DirToMove(dir);
     
-    if totalMoves ~= 0
-        moveStep = pathLength/totalMoves;
-        if mod(currentStep,moveStep) == 0   % Shift the belief map every N steps
-            tmp_map = noncircshift(map, move);
-            map = tmp_map ./sum(tmp_map(:));    % Scale it to 1
-        end
-    end
     
-    pSensorNoDetection = ones(mapSizeX,mapSizeY); % Initialize the probability of no detection with ones
-    pSensorNoDetection(location.y,location.x) = 0; % For binary sensor model, the probability of no detection at UAV location is zero
-    newMap = pSensorNoDetection .* map;   % Update the belief map
-    scaleFactor = sum(newMap(:));    % Calculate the scaling factor
-    newMap = newMap ./ scaleFactor;  % Scale the updated belief map   
 end
